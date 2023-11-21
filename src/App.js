5cb3faff4c5214b3d4715a6d8cf9e90446385321
Tim Garcia <timgarciaa@gmail.com>
2023-11-21 11:41:37 +0800

    Updated Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header></Header>
-
-      <main>
-
-      </main>
-
-      <footer>
-
-      </footer>
+      <Routing></Routing>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import About from "../components/About";
+import Menu from '../components/Menu';
+import Testi from '../components/Testi';
+import Description from '../components/Description';
+
+export default function Homepage() {
+  return (
+    <>
+        <About></About>
+        <Menu></Menu>
+        <Testi></Testi>
+        <Description></Description>
+    </>
+  );
+}
